3bb0802523bbcdd9963b2c23b596e400dd6a897e
GiaThinn <trangiathinh53@gmail.com>
2024-02-24 23:05:35 +0700

    [Location] - location pages

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'location_pages/location_home_page.dart';
+import 'location_pages/location_description.dart';
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
@@ -73,7 +75,7 @@ class MyApp extends StatelessWidget {
                     return const SampleItemDetailsView();
                   case SampleItemListView.routeName:
                   default:
-                    return const SampleItemListView();
+                    return const LocationDescription();
                 }
               },
             );
