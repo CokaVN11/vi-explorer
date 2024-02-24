c5db44f375b62f01d6c625c1b52c080d0a6ef824
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2024-02-24 22:10:21 +0700

    [main1] fix: Profile screen modifying

--- lib/screens/profile/profileScreen.dart
+++ lib/screens/profile/profileScreen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'PassportScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreenApp extends StatelessWidget {
   const ProfileScreenApp({super.key});
 
+  Future<void> clearSharedPreferences() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear(); // This will clear all data stored in SharedPreferences
+  }
+
   @override
   Widget build(BuildContext context) {
+    clearSharedPreferences();
     return const MaterialApp(
       title: 'Profile Screen',
       home: ProfileScreen(),
@@ -15,6 +22,7 @@ class ProfileScreenApp extends StatelessWidget {
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
+  static const routeName = '/profile';
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +63,6 @@ class ProfileScreen extends StatelessWidget {
               child: const Text('Edit Profile'),
             ),
             ElevatedButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => PassportScreen()));
-
             }, child: const Text('MY PASSPORT')),
           ],
         ),
